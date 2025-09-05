3f8fb74e59c37dd6ddc258d195850604eff28626
SebastianNCalvo <sebaacalvoo@gmail.com>
2025-09-05 19:28:49 -0300

    separando botones agregar

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-const arrayCarrito = []
+let arrayCarrito = []
+
 
 const cargarDomAlmacen = async () => {
     let traerArrayJson = await fetch("../json/productos.json")
@@ -19,26 +20,7 @@ const cargarDomAlmacen = async () => {
         `
     divProductos.appendChild(div);
     })
-    let botones = document.querySelectorAll(".button");
-    botones.forEach(btn => {
-        btn.addEventListener("click", (e)=>{
-            let id = e.target.id;
-            let prodEncontrado = nuevoArrayProductos.find(prod => prod.id == id)
-            Toastify({
-                text: "Producto agregado al carrito",
-                duration: 3000,
-                gravity: "bottom",
-                position: "center",
-                style: {
-                    fontSize: "15px",
-                    background: "linear-gradient(to right, #38b000ff, #3dc944ff)",
-                    borderRadius:"25px"
-                },
-            }).showToast();
-            arrayCarrito.push(prodEncontrado)
-            localStorage.setItem("Carrito", JSON.stringify(arrayCarrito))          
-        })
-    })
+    botonAgregar()
 }
 
 const trayendoArrayJson = async () =>{
@@ -62,157 +44,39 @@ const trayendoArrayJson = async () =>{
                 `;
             divProductos.appendChild(div)
         })
-        let botones = document.querySelectorAll(".button");    
-        botones.forEach(btn => {
-            btn.addEventListener("click", (e)=>{
-                let id = e.target.id;
-                let prodEncontrado = nuevoArrayProductos.find(prod => prod.id == id)
-                Toastify({
-                    text: "Producto agregado al carrito",
-                    fontSize: "1.8 rem",
-                    duration: 3000,
-                    gravity: "bottom",
-                    position: "center",
-                    style: {
-                        background: "linear-gradient(to right, #38b000ff, #3dc944ff)",
-                        borderRadius:"5px"
-                    },
-                }).showToast();
+        botonAgregar()
+    })
+}
+
+const botonAgregar = async () => {
+    let traerArrayJson = await fetch("../json/productos.json")
+    let nuevoArrayProductos = await traerArrayJson.json();
+    let botones = document.querySelectorAll(".button");    
+    botones.forEach(btn => {
+        btn.addEventListener("click", (e)=>{
+            let id = e.target.id;
+            let prodEncontrado = nuevoArrayProductos.find(prod => prod.id == id)
+            Toastify({
+                text: "Producto agregado al carrito",
+                fontSize: "1.8 rem",
+                duration: 3000,
+                gravity: "bottom",
+                position: "center",
+                style: {
+                    background: "linear-gradient(to right, #38b000ff, #3dc944ff)",
+                    borderRadius:"5px"
+                },
+            }).showToast();
+            if(arrayCarrito.find(prod => prod == prodEncontrado)) {
+                prodEncontrado.cantidad = prodEncontrado.cantidad +1
+                sessionStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
+            } else {
                 arrayCarrito.push(prodEncontrado)
-                localStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
-            })
+                sessionStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
+            }
         })
     })
 }
 
 trayendoArrayJson()
 cargarDomAlmacen()
-
-// cargarDomAlmacen()
-
-// let buscador = document.querySelector("#buscador")
-// buscador.addEventListener("keyup", (e) =>{
-//     let divProductos = document.querySelector("#divProductos");
-//     divProductos.innerHTML = ""
-//     arrayProductos.filter(producto => producto.nombre.toLowerCase().includes(e.target.value)).forEach(producto => {
-//         let div = document.createElement("div");
-//         div.className = "col-xl-3 col-lg-3 col-md-6 col-sm-6 col-xs-6"
-//         div.innerHTML = `
-//             <div class="cajita">
-//                 <img class="img_shadow" src="${producto.img}" alt="">
-//                 <p class="descripcion-fotos">${producto.nombre}</p>
-//                 <span>$${producto.precio}</span>
-//                 <button class="button" data-id="${producto.id}">Agregar</button>
-//             </div>
-//             `;
-//         divProductos.appendChild(div)
-//     })
-//     let botones = document.querySelectorAll(".button");
-//     botones.forEach((boton) => {
-//         boton.addEventListener("click", (e) => {
-//             let id = e.target.arrayProductosset.id;
-//             let prodEncontrado = arrayProductos.find((producto) => producto.id == id);
-//             console.log(`Producto encontrado: ${prodEncontrado}`)
-//         })
-//     })
-// })
-
-// let seleccionaAlfajorMilka = document.querySelector("#milka-oreo");
-// let seleccionaAlfajorPepitos = document.querySelector("#pepito-triple");
-// let seleccionaAlfajorTerrabusi = document.querySelector("#terrabusi");
-// let seleccionaAlfajorShot = document.querySelector("#shot-con-mani");
-
-// let seleccionaCoflerAlmendras = document.querySelector("#cofler-almendras");
-// let seleccionaCoflerRocklets = document.querySelector("#cofler-rocklets");
-// let seleccionaCoflerBlock = document.querySelector("#cofler-block");
-// let seleccionaCoflerTresPlaceres = document.querySelector("#cofler-tres-placeres");
-
-// let seleccionaSnackJamonSerrano = document.querySelector("#jamon-serrano");
-// let seleccionaSnackKetchup = document.querySelector("#ketchup")
-// let seleccionaSnackPehuamarAcanaladas = document.querySelector("#pehuamar-acanaladas")
-// let seleccionaSnackPehuamarClasicas = document.querySelector("#pehuamar-clasicas")
-
-// let seleccionaPizzaMozzarella = document.querySelector("#pizza-muzzarella-2")
-// let seleccionaPizzaMozzarellaXL = document.querySelector("#pizza-mozzarella-XL")
-// let seleccionaPizzaMozzarellaClasica = document.querySelector("#pizza-mozzarella-clasica")
-// let seleccionaPizzaMozzarellaJYQ = document.querySelector("#pizza-mozzarella-jyq")
-
-// seleccionaAlfajorMilka.addEventListener('click', function(){
-//     arrayCarrito.push(alfajorMilka)
-//     localStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
-// })
-
-// seleccionaAlfajorPepitos.addEventListener('click', function(){
-//     arrayCarrito.push(alfajorPepitos)
-//     localStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
-// })
-
-// seleccionaAlfajorTerrabusi.addEventListener('click', function(){
-//     arrayCarrito.push(alfajorTerrabusi)
-//     localStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
-// })
-
-// seleccionaAlfajorShot.addEventListener('click', function(){
-//     arrayCarrito.push(alfajorShot)
-//     localStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
-// })
-
-// seleccionaCoflerAlmendras.addEventListener('click', function(){
-//     arrayCarrito.push(coflerAlmendras)
-//     localStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
-// })
-
-// seleccionaCoflerBlock.addEventListener('click', function(){
-//     arrayCarrito.push(coflerBlock)
-//     localStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
-// })
-
-// seleccionaCoflerRocklets.addEventListener('click', function(){
-//     arrayCarrito.push(coflerRocklets)
-//     localStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
-// })
-
-// seleccionaCoflerTresPlaceres.addEventListener('click', function(){
-//     arrayCarrito.push(coflerTresPlaceres)
-//     localStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
-// })
-
-// seleccionaSnackJamonSerrano.addEventListener('click', function(){
-//     arrayCarrito.push(snackJamonSerrano)
-//     localStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
-// })
-
-// seleccionaSnackKetchup.addEventListener('click', function(){
-//     arrayCarrito.push(snackKetchup)
-//     localStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
-// })
-
-// seleccionaSnackPehuamarAcanaladas.addEventListener('click', function(){
-//     arrayCarrito.push(snackPehuamarAcanaladas)
-//     localStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
-// })
-
-// seleccionaSnackPehuamarClasicas.addEventListener('click', function(){
-//     arrayCarrito.push(snackPehuamarClasicas)
-//     localStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
-// })
-
-// seleccionaPizzaMozzarella.addEventListener('click', function(){
-//     arrayCarrito.push(pizzaMozzarella)
-//     localStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
-// })
-
-// seleccionaPizzaMozzarellaClasica.addEventListener('click', function(){
-//     arrayCarrito.push(pizzaMozzarellaClasica)
-//     localStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
-// })
-
-// seleccionaPizzaMozzarellaJYQ.addEventListener('click', function(){
-//     arrayCarrito.push(pizzaMozzarellaJYQ)
-//     localStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
-// })
-
-// seleccionaPizzaMozzarellaXL.addEventListener('click', function(){
-//     arrayCarrito.push(pizzaMozzarellaXL)
-//     localStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
-// })
--- js/appCarrito.js
+++ js/appCarrito.js
@@ -1,25 +1,68 @@
-const arrayCarrito = JSON.parse(localStorage.getItem("Carrito"))
-console.log(arrayCarrito)
 let total = 0
-let cantidad = 1
-
 
 const cargarDomCarrito = () => {
-    let tablaCarrito = document.querySelector("#tablaCarrito");
-    arrayCarrito.forEach(producto => {
-        let tabla = document.createElement("tr");
-        total = total + producto.precio
-        tabla.innerHTML = `                
+    try {
+        let arrayCarrito = JSON.parse(sessionStorage.getItem("Carrito"))
+        let tablaCarrito = document.querySelector("#tablaCarrito");
+        let tablaTitulos = document.createElement("tr")
+        tablaTitulos.innerHTML = `
             <tr>
-                <td><img class="img_shadow" src="${producto.img}" alt=""></td>
-                <td><h2>${producto.nombre}</h2></td>
-                <td><h2>${cantidad}</h2></td>
-                <td><h2>${producto.precio}</h2></td>
+                <th>Imagen</th>
+                <th>Nombre</th>
+                <th>Precio c/u</th>
+                <th>Cantidad</th>
+                <th>Eliminar</th>
+                <th>Precio total</th>
             </tr>
-            `;
-        tablaCarrito.appendChild(tabla)
-    });
+        `
+        tablaCarrito.appendChild(tablaTitulos)
+        arrayCarrito.forEach(producto => {
+            let tablaProductos = document.createElement("tr");
+            let precioTotal = producto.precio * producto.cantidad
+            total = total + precioTotal
+            tablaProductos.innerHTML = `                
+                <tr class="listaCarrito">
+                    <td><img class="img_shadow" src="${producto.img}" alt=""></td>
+                    <td><h2>${producto.nombre}</h2></td>
+                    <td><h2>${producto.precio}</h2></td>
+                    <td><h2>${producto.cantidad}</h2></td>
+                    <td><button class="btnEliminar" id=${producto.id}>Eliminar</button></td>
+                    <td><h2>${precioTotal}</h2></td>
+                </tr>
+                `;
+            tablaCarrito.appendChild(tablaProductos)
+        });
+        let botones = document.querySelectorAll(".btnEliminar");
+        botones.forEach(btn => {
+            btn.addEventListener("click", (e)=>{
+                let id = e.target.id;
+                let prodEncontrado = arrayCarrito.find(prod => prod.id == id)
+                if(arrayCarrito.find(prod => prod == prodEncontrado)) {
+                    prodEncontrado.cantidad = prodEncontrado.cantidad -1
+                    total = 0
+                    let tablaCarrito = document.querySelector("#tablaCarrito");
+                    tablaCarrito.innerHTML =""
+                    if(prodEncontrado.cantidad == 0){
+                        arrayCarrito = arrayCarrito.filter(prod => prod.cantidad > 0)
+                    }
+                    sessionStorage.setItem("Carrito", JSON.stringify(arrayCarrito))
+                    let tablaTotales = document.querySelector("#tablaTotales")
+                    tablaTotales.innerHTML= ""
+                    cargarDomCarrito()
+                }
+            })
+        })
+        cargarTotal()
+    } catch (error) {        
+        let contenedorDeTitulos = document.querySelector(".contenedorDeTitulos")
+        let h4 = document.createElement("h4")
+        h4.innerHTML=`
+        <h4>Su carrito se encuentra vacío en este momento. Agregue los productos deseados</h4>
+        `
+        contenedorDeTitulos.appendChild(h4)
+    }
 }
+
 
 const cargarTotal = () => {
     let totalCarrito = document.querySelector("#tablaTotales");
@@ -35,4 +78,3 @@ const cargarTotal = () => {
 }
 
 cargarDomCarrito()
-cargarTotal()
